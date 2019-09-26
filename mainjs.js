eb3506ec77b6df814a95b35a9201d10058ef3b9e
IVYMAX <ivyli@IVYMAXmbp.lan>
2019-09-26 13:01:49 +1000

    add css file

--- mainjs.js
+++ mainjs.js
@@ -2,16 +2,19 @@ window.onload = function() {
   document.getElementById("button").addEventListener("click", addStep);
 
   function addStep() {
+    //get the input date from form
     var Robot1StartPosition1 = document.getElementById("robotStartPosition")
       .value;
     var Robot1MoveStep = document.getElementById("robotMoveStep").value;
     var landsize = document.getElementById("landingsize").value;
     var Stoppositon = {};
 
+    //change the input data to Array
     var Robot1StartPosition1Array = Robot1StartPosition1.split(" ");
     var Robot1MoveStepArray = Robot1MoveStep.split("");
     var landsizeArray = landsize.split(" ");
 
+    //set the facing side to a value for calculate
     function setposition() {
       value = Robot1StartPosition1Array[2];
 
@@ -27,10 +30,12 @@ window.onload = function() {
     }
     setposition();
 
+    //set the input position string data to number
     var x = Number(Robot1StartPosition1Array[0]);
     var y = Number(Robot1StartPosition1Array[1]);
     var s = Robot1StartPosition1Array[2];
 
+    //robot moving step
     for (i = 0; i < Robot1MoveStepArray.length; i++) {
       var NewFaceSide = Number(s % 4);
 
@@ -89,13 +94,14 @@ window.onload = function() {
           }
         }
       }
-
+      //check each step result
       console.log(x);
       console.log(y);
       console.log(s);
       console.log("--");
     }
 
+    //change to faceting value to string
     function StopPosition() {
       if (NewFaceSide == "3") {
         return (Stoppositon[2] = "W");
@@ -115,9 +121,11 @@ window.onload = function() {
     }
     StopPosition();
 
+    //set stop position
     Stoppositon[0] = x;
     Stoppositon[1] = y;
 
+    //show output
     document.getElementById("robotStopPosition").innerHTML =
       "<P>" +
       Stoppositon[0] +
